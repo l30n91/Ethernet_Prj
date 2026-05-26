ba4f43f3ae57b4c60f0a32e22dafdf154da9a0fb
l30n491 <leonardo.buonaiuto91@gmail.com>
2026-05-26 10:51:06 +0200

    added ignore rules

--- MDK-ARM/STM32F429ZI_NUCLEO_144/startup_stm32f429xx.d
+++
@@ -1,1 +0,0 @@
-stm32f429zi_nucleo_144\startup_stm32f429xx.o: startup_stm32f429xx.s
--- STM32CubeIDE/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- E:/T1893PFW_00/STM32CubeF4/Drivers/BSP/Components/lan8742/lan8742.c \
- E:/T1893PFW_00/STM32CubeF4/Drivers/BSP/Components/lan8742/lan8742.h
-E:/T1893PFW_00/STM32CubeF4/Drivers/BSP/Components/lan8742/lan8742.h:
